8c4c3b00b0a4301626a053ea097697098596a97d
nanorocks <andrejnankov@gmail.com>
2025-09-02 00:30:56 +0200

    Hotfix migration file missing

--- src/LicenseManagerServiceProvider.php
+++ src/LicenseManagerServiceProvider.php
@@ -4,22 +4,17 @@ namespace Nanorocks\LicenseManager;
 
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use Nanorocks\LicenseManager\Commands\RevokeLicenseCommand;
 use Nanorocks\LicenseManager\Commands\GenerateLicenseCommand;
+use Nanorocks\LicenseManager\Commands\RevokeLicenseCommand;
 
 class LicenseManagerServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('license-manager')
-            ->hasConfigFile()
-            ->hasMigration('create_licenses_table')
+            ->hasConfigFile() // Publishes config/license-manager.php
+            ->hasMigration('create_licenses_table') // Publishes migration
             ->hasCommand(GenerateLicenseCommand::class)
             ->hasCommand(RevokeLicenseCommand::class);
     }
@@ -27,18 +22,21 @@ class LicenseManagerServiceProvider extends PackageServiceProvider
     public function boot(): void
     {
         if ($this->app->runningInConsole()) {
-            $this->commands([
-                \Nanorocks\LicenseManager\Commands\GenerateLicenseCommand::class,
-            ]);
+            // Publishes only the config
             $this->publishes([
                 __DIR__ . '/../config/license-manager.php' => config_path('license-manager.php'),
             ], 'license-manager-config');
+
+            // Publishes the specific migration
+            $this->publishes([
+                __DIR__ . '/../database/migrations/2025_09_01_000000_create_licenses_table.php' => database_path('migrations/2025_09_01_000000_create_licenses_table.php'),
+            ], 'license-manager-migrations');
         }
     }
 
     public function register(): void
     {
-        // Merge config from package root config folder
+        // Merge config from package root
         $this->mergeConfigFrom(
             __DIR__ . '/../config/license-manager.php',
             'license-manager'
